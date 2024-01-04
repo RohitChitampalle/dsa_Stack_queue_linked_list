0256d8a89af2817396e72c432733cd97c58f2d5b
Rohit Chitampalle <chitampalle813@gmail.com>
2024-01-04 09:28:02 +0530

    changes done in linked list ..

--- linked list DSA/basicOFLinkedList.js
+++ linked list DSA/basicOFLinkedList.js
@@ -15,18 +15,22 @@ class Node {
 
     insertAtHead(head, data) {
         let temp = new Node(data)
-        temp.next = head
-        head = temp
-        return head
+        let current;
+        if (head.next == null) {
+            head.next = temp
+        } else {
+            current = head
+
+            while (current.next) {
+                current = current.next
+            }
+            current.next = temp
+        }
     }
     insertAtTail(tail, data) {
         let temp = new Node(data)
-        while(tail.next != null){
-            
-            temp=tail.next
-        }
         tail.next = temp
-        return temp
+        // return temp
     }
 
     print_heade(head) {
@@ -39,49 +43,79 @@ class Node {
     }
 
     insertAtPosition(head, position, data) {
-        let temp = JSON.parse(JSON.stringify(head));
-        let cnt = 1;
-//inserting at 1 position
-        if(position == 1){
-             temp=this.insertAtHead(head,data)
-             head=temp
-            return head
-        }
-        //inserting at mid position
-
-        while (cnt <  position - 1) {
-            temp = temp.next
-            cnt++
-        }
-
-        //inserting at last position
-        if(temp.next == null){
-            temp.next=this.insertAtTail(tail,data)
-            head = temp
+        if (position <= 0) {
+            console.log("Please enter a valid index.");
             return
         }
-        let nodeToInsert =  new Node(data)
-        nodeToInsert.next=temp.next
-        temp.next = nodeToInsert
+        let temp = new Node(data);
+        let cnt = 0;
+        let cur, prev
+        //inserting at 1 position
+        if (position == 1) {
+            temp.next = head;
+            head = temp;
+        } else {
+            cur = head
 
-        // temp.next = nodeToInsert
+            while (cnt < position - 1) {
+                prev = cur
+                cur = cur.next
+                cnt++
+            }
+            temp.next = cur
+            prev.next = temp
+        }
     }
+
+    deleteNode(position, head) {
+        if (position == 1) {
+            let temp = head
+            head = head.next
+            temp.next = null
+            // return head
+
+        } else {
+            //deleting any middle node and last node
+            let cur = head
+            let prev = null
+
+            let cnt = 1;
+            while (cnt < position) {
+                prev = cur
+                cur = cur.next
+                cnt++
+            }
+            prev.next = cur.next
+            cur.next = null
+        }
+    }
+
 }
 
 
 let node1 = new Node(10);
-console.log(node1.data)
-console.log(node1.next)
 head = node1
 tail = node1
-console.log("Head before is ->", head)
-let h1 = node1.insertAtTail(tail, 12)
+// console.log("Head before is ->", head)
+node1.print_heade(head)
 console.log("----x----x----x----x----x----x----")
-let h2 = node1.insertAtTail(h1, 13)
-let h3 = node1.insertAtTail(h2, 14)
-node1.insertAtPosition(head,5,50)
+node1.insertAtHead(head, 12)
+node1.insertAtHead(head, 13)
+node1.insertAtHead(head, 14)
+node1.insertAtHead(head, 15)
+// node1.insertAtPosition(head, 2, 50)
 node1.print_heade(head)
 console.log("--------x----x----x----x--------")
-let h4 = node1.insertAtTail(h3, 15)
-let h5 = node1.insertAtTail(h4, 16)
+node1.deleteNode(2, head)
 node1.print_heade(head)
+
+
+
+
+
+
+
+
+// node1.print_heade(d1)
+// let h5 = node1.insertAtTail(h4, 16)
+// node1.print_heade(head)
