755b734ed23f0722cd3fc21569d93a20ce2bfc0f
Rohit Chitampalle <chitampalle813@gmail.com>
2024-01-05 10:06:50 +0530

    merge sorting in linked in

--- linked list DSA/singlyLinkedList.js
+++ linked list DSA/singlyLinkedList.js
@@ -89,41 +89,134 @@ class Node {
             cur.next = null
         }
     }
-    reverseLinkedList(head) {
-        var prev = null;
-        var current = head;
-        var next = null;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+    // reverseLinkedList(head) {
+    //     var prev = null;
+    //     var current = head;
+    //     var next = null;
+    //     while (current != null) {
+    //         next = current.next;
+    //         current.next = prev;
+    //         prev = current;
+    //         current = next;
+    //     }
+    //     head = prev;
+    //     this.print_heade(head)
+    // }
+    lenthOfLinkedList(head) {
+        let cnt = 0;
+        let temp = head
+        while (temp != null) {
+            temp = temp.next
+            cnt++
         }
-        head = prev;
-        this.print_heade(head)
+        return cnt
+
+    }
+
+    middleNode(head) {
+        let length = this.lenthOfLinkedList(head)
+        let middle = Math.floor(length / 2)
+        let temp = head
+        let cnt = 0;
+        while (cnt < middle) {
+
+            temp = temp.next
+            cnt++
+        }
+
+        return temp
+
+    }
+
+    merge(head1, head2) {
+        var merged = new Node(-1);
+        var temp = merged;
+
+        // While head1 is not null and head2
+        // is not null
+        while (head1 != null && head2 != null) {
+            if (head1.data < head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        // While head1 is not null
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        // While head2 is not null
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return merged.next;
+    } 
+
+    findMid(head) {
+        var slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    mergeSort(head) {
+
+        if (head.next == null)
+            return head;
+
+        let middle =this.findMid(head)
+
+        var head2 = middle.next;
+        middle.next = null;
+        var newHead1 = this.mergeSort(head);
+        var newHead2 = this.mergeSort(head2);
+        var finalHead = this.merge(newHead1, newHead2);
+
+        return finalHead;
     }
 
 }
 
+var head = new Node(7);
+var temp = head;
+temp.next = new Node(10);
+temp = temp.next;
+temp.next = new Node(5);
+temp = temp.next;
+temp.next = new Node(20);
+temp = temp.next;
+temp.next = new Node(3);
+temp = temp.next;
+temp.next = new Node(2);
+temp = temp.next;
 
-let node1 = new Node(10);
-head = node1
-tail = node1
-// console.log("Head before is ->", head)
-node1.print_heade(head)
-console.log("----x----x----x----x----x----x----")
-node1.insertAtHead(head, 12)
-node1.insertAtHead(head, 13)
-node1.insertAtHead(head, 14)
-node1.insertAtHead(head, 15)
-// node1.insertAtPosition(head, 2, 50)
+// Apply merge Sort
+head =head.mergeSort(head);
+// let node1 = new Node(1);
+// head = node1
+// tail = node1
+// // console.log("Head before is ->", head)
+// // console.log("----x----x----x----x----x----x----")
+// node1.insertAtHead(head, 3)
+// node1.insertAtHead(head, 4)
+// node1.insertAtHead(head, 2)
+// node1.insertAtHead(head, 5)
+
 // node1.print_heade(head)
-// node1.deleteNode(2, head)
-node1.print_heade(head)
-console.log("--------x----x----x----x--------")
-node1.reverseLinkedList(head)
-
-
+// console.log("--------x----x----x----x--------")
+// let resul = node1.mergeSort(head)
+// console.log(resul)
 
 
 
