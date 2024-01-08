b86a1deef422e7ea09809d49559d65969790b979
Rohit Chitampalle <chitampalle813@gmail.com>
2024-01-08 09:47:47 +0530

    Segregate even and odd nodes in a Linked List

--- linked list DSA/singlyLinkedList.js
+++ linked list DSA/singlyLinkedList.js
@@ -370,17 +370,75 @@ class Node {
         }
         this.print_heade(head)
     }
+    segregateEvenOdd() {
+        var end = head;
+        var prev = null;
+        var curr = head;
+
+        /* Get pointer to last Node */
+        while (end.next != null)
+            end = end.next;
+
+        var new_end = end;
+
+        // Consider all odd nodes before getting first even node 
+        while (curr.data % 2 != 0 && curr != end) {
+            new_end.next = curr;
+            curr = curr.next;
+            new_end.next.next = null;
+            new_end = new_end.next;
+        }
+
+        // do following steps only if there is an even node 
+        if (curr.data % 2 == 0) {
+            head = curr;
+
+            // now curr points to first even node 
+            while (curr != end) {
+                if (curr.data % 2 == 0) {
+                    prev = curr;
+                    curr = curr.next;
+                } else {
+                    /* Break the link between prev and curr */
+                    prev.next = curr.next;
+
+                    /* Make next of curr as null */
+                    curr.next = null;
+
+                    /* Move curr to end */
+                    new_end.next = curr;
+
+                    /* Make curr as new end of list */
+                    new_end = curr;
+
+                    /* Update curr pointer */
+                    curr = prev.next;
+                }
+            }
+        }
+
+        /* We have to set prev before executing rest of this code */
+        else
+            prev = curr;
+
+        if (new_end != end && end.data % 2 != 0) {
+            prev.next = end.next;
+            end.next = null;
+            new_end.next = end;
+        }
+    }
 }
 
 
 let node1 = new Node(6);
 head = node1
-node1.insertAtHead(head, 4)
-node1.insertAtHead(head, 9)
 node1.insertAtHead(head, 5)
 node1.insertAtHead(head, 7)
+node1.insertAtHead(head, 4)
+node1.insertAtHead(head, 9)
 
-node1.updateList(head,2)
+// node1.updateList(head,2)
+node1.segregateEvenOdd()
 // node1.pairWiseSwap(head)
 // node1.print_heade(head)
 console.log("----xx----xx----xx----xxx----")
