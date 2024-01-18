f75514c053dedc5d8adedabc5d65cc26c9109272
Rohit Chitampalle <chitampalle813@gmail.com>
2024-01-18 19:30:34 +0530

    level order linked list and pattern code in javascript

---
+++ pattern_problems/pattern.js
@@ -0,0 +1,81 @@
+//! left half pyramid pattern code 
+// *
+// **
+// ***
+// ****
+// *****
+
+// let star=""
+
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=i;j++){
+//         star+="*"
+//     }
+//     star += "\n"
+// }
+// console.log(star)
+
+//!left invert half pyramid pattern code 
+// *****
+// ****
+// ***
+// **
+// *
+
+// let star = ""
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         star += "*"
+//     }
+//     star += "\n"
+// }
+// console.log(star.split('').reverse().join(''))
+
+//!right  half pyramid pattern code 
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// let star = ""
+
+// for (let i = 1; i <= 5; i++) {
+
+//     for (let j = 0; j < 5 - i; j++) {
+//         star += " ";
+//     }
+//     for (let k = 0; k < i; k++) {
+//         star += "*";
+//     }
+//     // printing star
+//     star += "\n";
+
+// }
+
+// console.log(star)
+
+//!right invert half pyramid pattern code 
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+// let star = ""
+
+// for (let i = 1; i <= 5; i++) {
+
+//         for (let j = 0; j <  i-1; j++) {
+//             star += " ";
+//         }
+//         for (let k = 0; k < 5-i+1; k++) {
+//             star += "*";
+//         }
+//         // printing star
+//         star += "\n";
+    
+// }
+
+// console.log(star)
--- trees/level_order_tree.js
+++ trees/level_order_tree.js
@@ -1,47 +1,47 @@
-// Iterative Queue based javascript program
-// to do level order traversal
-// of Binary Tree
+// // Iterative Queue based javascript program
+// // to do level order traversal
+// // of Binary Tree
 
-// Class to represent Tree node
-class Node {
-    constructor(val) {
-        this.data = val;
-        this.left = null;
-        this.right = null;
-    }
-}
+// // Class to represent Tree node
+// class Node {
+//     constructor(val) {
+//         this.data = val;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
 
-// Given a binary tree. Print its nodes 
-// in level order using array for implementing queue
-function printLevelOrder() {
-    var queue = [];
-    queue.push(root);
-    while (queue.length != 0) {
+// // Given a binary tree. Print its nodes 
+// // in level order using array for implementing queue
+// function printLevelOrder() {
+//     var queue = [];
+//     queue.push(root);
+//     while (queue.length != 0) {
 
-        // The shift() method removes 
-        // the first element from an array 
-        // and returns that removed element.
-        var tempNode = queue.shift();
-        console.log(tempNode.data + " ");
+//         // The shift() method removes 
+//         // the first element from an array 
+//         // and returns that removed element.
+//         var tempNode = queue.shift();
+//         console.log(tempNode.data + " ");
 
-        // Enqueue left child
-        if (tempNode.left != null) {
-            queue.push(tempNode.left);
-        }
+//         // Enqueue left child
+//         if (tempNode.left != null) {
+//             queue.push(tempNode.left);
+//         }
 
-        // Enqueue right child
-        if (tempNode.right != null) {
-            queue.push(tempNode.right);
-        }
-    }
-}
+//         // Enqueue right child
+//         if (tempNode.right != null) {
+//             queue.push(tempNode.right);
+//         }
+//     }
+// }
 
-// creating a binary tree and entering the nodes
-var root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-console.log("Level order traversal of binary tree is - ");
-printLevelOrder();
+// // creating a binary tree and entering the nodes
+// var root = new Node(1);
+// root.left = new Node(2);
+// root.right = new Node(3);
+// root.left.left = new Node(4);
+// root.left.right = new Node(5);
+// console.log("Level order traversal of binary tree is - ");
+// printLevelOrder();
 
